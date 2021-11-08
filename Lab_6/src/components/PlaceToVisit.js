201560ba19ebfddc7c04abc466c100ef9f05fc1f
Taras.Hrudzinskyi <polniyzopa@gmail.com>
2021-11-08 04:38:57 +0300

    Added files for 6 lab

---
+++ Lab_6/src/components/PlaceToVisit.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import ImageCard from './ImageCard';
+import places from '../static/places';
+
+const useStyles = makeStyles((theme) => ({
+    root:{
+        minHeight: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+}));
+export default function () {
+    const classes = useStyles();
+    return (
+        <div className={classes.root}>
+            <ImageCard place={places[0]} />
+            <ImageCard place={places[1]} />
+        </div>
+    );
+}
